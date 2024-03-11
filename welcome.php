c5e3567d2d32561ff05447febe90f53f25abfb40
NatzNatz <125758574+QQUway@users.noreply.github.com>
2024-03-11 23:58:49 +0700

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+
+<div>
+    <h2>Welcome to the System!</h2>
+    <p><a href="logout.php">Logout</a></p>
+</div>
+
+</body>
+</html>

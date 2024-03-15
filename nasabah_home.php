06574de10716847e4cb2b6305ba0238edeb0cfc9
NatzNatz <125758574+QQUway@users.noreply.github.com>
2024-03-15 21:17:01 +0700

    gw gatau ini isi apa

--- nasabah_home.php
+++ nasabah_home.php
@@ -12,12 +12,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="resource\css\nasabah.css">
+
+
 </head>
 <body>
 
 <div>
     <h2>Welcome to the System!</h2>
-    <p><a href="logout.php">Logout</a></p>
+    <iframe src="https://show.co/social-unlock/6cq55VMPFBC1WdFCXolYCc/widget" width="1920" height="1080" frameborder="0"></iframe>
+    <h2><a href="logout.php">Logout</a></h2>
 </div>
 
 </body>

b0dfcda2a09367631ae118cdf26e059b1c102fe8
hawyeet <53439203+hawyeet@users.noreply.github.com>
2024-03-16 08:45:52 +0700

    updated navbar

---
+++ users.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="resource/css/navbar-footer.css">
+    <title>Users List</title>
+</head>
+
+<body>
+    <div class="navbar">
+        <a href="#">Home</a>
+        <a href="#">Users</a>
+        <a href="#">History</a>
+        <a href="#">Log Out</a>
+    </div>
+
+    <div class="footer">
+        <p>&copy; 2024 KDHH Koperasi. All rights reserved.</p>
+    </div>
+</body>
+
+</html>
